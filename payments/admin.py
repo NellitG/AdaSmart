25691ff9e2ba844d2d8df5df87bf70beccba9ca5
NellitG <kamaunelson157@gmail.com>
2025-07-08 17:46:18 +0300

    Register FeePayment model in admin with custom display for receipt

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import FeePayment
+from django.utils.html import format_html
 
 # Register your models here.
+@admin.register(FeePayment)
+class FeePaymentAdmin(admin.ModelAdmin):
+    list_display = ('student_name', 'amount_paid', 'payment_date', 'payment_method', 'view_receipt')
+
+    def view_receipt(self, obj):
+        if obj.receipt_url:
+            return format_html(f'<a href="{obj.receipt_url}" target="_blank">Download</a>')
+        return "-"
