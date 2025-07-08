d32e01505918134a2e7e0315470696fca0b9ffa0
NellitG <kamaunelson157@gmail.com>
2025-07-08 14:39:12 +0300

    Add ready function to PaymentsConfig to import signals

--- payments/apps.py
+++ payments/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 
+def ready(self):
+    import payments.signals
+   
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payments'
