780f0dd51cf20f338a04411d48584b1a8f9bd39e
NellitG <kamaunelson157@gmail.com>
2025-07-08 14:26:06 +0300

    Add User model with role choices for admin, teacher, student, and parent

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create an AbstractUser
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+        ('parent', 'Parent'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
