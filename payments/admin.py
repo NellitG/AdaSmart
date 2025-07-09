bbe1c45d271b464ae439c1667f525f9155c4ff56
NellitG <kamaunelson157@gmail.com>
2025-07-09 18:36:00 +0300

    Added FeePayment and FeeBalance models with admin interfaces

--- payments/admin.py
+++ payments/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import FeePayment
+from .models import FeePayment, FeeBalance
 from django.utils.html import format_html
+from users.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 
 # Register your models here.
 @admin.register(FeePayment)
@@ -11,3 +14,22 @@ class FeePaymentAdmin(admin.ModelAdmin):
         if obj.receipt_url:
             return format_html(f'<a href="{obj.receipt_url}" target="_blank">Download</a>')
         return "-"
+    
+@admin.register(FeeBalance)
+class FeeBalanceAdmin(admin.ModelAdmin):
+    list_display = ('student_name', 'total_fee', 'paid_fee', 'due_fee')
+
+    def student_name(self, obj):
+        return f"{obj.student.first_name} {obj.student.last_name}"
+
+    student_name.short_description = 'Student Name'
+    
+    def total_fee(self, obj):
+        return obj.total_fee
+
+    def paid_fee(self, obj):
+        return obj.paid_fee
+
+    def due_fee(self, obj):
+        return obj.due_fee
+
