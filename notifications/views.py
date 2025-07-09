68f1bba297fdad2aba36dee734da56c8f42eb1ae
NellitG <kamaunelson157@gmail.com>
2025-07-09 18:34:22 +0300

    Add view to send SMS reminders for fee balances

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from payments.models import FeeBalance
+from .services import send_sms_notification
 
-# Create your views here.
+def send_debt_reminders(request):
+    count = 0
+    for balance in FeeBalance.objects.all():
+        if balance.balance > 0:
+            message = f"Dear Parent, your fee balance is KES {balance.balance}. Please clear it soon."
+            send_sms_notification(balance.parent.phone_number, message)
+            count += 1
+    return JsonResponse({'status': 'reminders sent', 'total': count})
+
+# Git commit message:
+# "Add view to send SMS reminders for fee balances"
