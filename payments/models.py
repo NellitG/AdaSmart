f30a6c269720783d12299c6fbad70622de09c70f
NellitG <kamaunelson157@gmail.com>
2025-07-09 18:39:57 +0300

    Create models for fee payments and balances

--- payments/models.py
+++ payments/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 from users.models import User
 
+
 # Create your models here.
-# Create a fee payment model
+
 class FeePayment(models.Model):
-    parent = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'parent'})
-    student_name = models.CharField(max_length=100)
+    parent = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'parent'},
+        related_name='payments_as_parent'
+    )
+    student_name = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'student'},
+        related_name='payments_as_student'
+    )
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
-    payment_method = models.CharField(max_length=50)
-    payment_date = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=20)
+    payment_date = models.DateField()
     receipt_url = models.URLField(blank=True, null=True)
+
 
     def __str__(self):
         return f"{self.student_name} - {self.amount_paid} KES"
