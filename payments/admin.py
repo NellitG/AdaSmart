13cfe14cbdee2351204bc13108edca3f343a866d
NellitG <kamaunelson157@gmail.com>
2025-07-10 12:25:01 +0300

    remove the receipt function and unneccessary import

--- payments/admin.py
+++ payments/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import FeePayment, FeeBalance
-from django.utils.html import format_html
 from users.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
@@ -8,12 +7,8 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
 @admin.register(FeePayment)
 class FeePaymentAdmin(admin.ModelAdmin):
-    list_display = ('student_name', 'amount_paid', 'payment_date', 'payment_method', 'view_receipt')
+    list_display = ('parent', 'amount_paid', 'payment_date', 'payment_method',)
 
-    def view_receipt(self, obj):
-        if obj.receipt_url:
-            return format_html(f'<a href="{obj.receipt_url}" target="_blank">Download</a>')
-        return "-"
     
 @admin.register(FeeBalance)
 class FeeBalanceAdmin(admin.ModelAdmin):
