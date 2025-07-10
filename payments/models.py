6851aa10ee64b3bb6b12a93a77b5042cc0656c20
NellitG <kamaunelson157@gmail.com>
2025-07-10 12:25:30 +0300

    remove the receipt url

--- payments/models.py
+++ payments/models.py
@@ -20,9 +20,7 @@ class FeePayment(models.Model):
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     payment_method = models.CharField(max_length=20)
     payment_date = models.DateField()
-    receipt_url = models.URLField(blank=True, null=True)
-
-
+    
     def __str__(self):
         return f"{self.student_name} - {self.amount_paid} KES"
 
