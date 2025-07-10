c10c9f3bd8ce5db307936d16a1d7f4cfa2165567
NellitG <kamaunelson157@gmail.com>
2025-07-10 14:09:05 +0300

    modify model payments

--- payments/models.py
+++ payments/models.py
@@ -1,28 +1,15 @@
 from django.db import models
-from users.models import User
-
-
-# Create your models here.
+from users.models import Student, User
 
 class FeePayment(models.Model):
-    parent = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        limit_choices_to={'role': 'parent'},
-        related_name='payments_as_parent'
-    )
-    student_name = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        limit_choices_to={'role': 'student'},
-        related_name='payments_as_student'
-    )
+    parent = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'parent'}, related_name='payments_as_parent')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     payment_method = models.CharField(max_length=20)
     payment_date = models.DateField()
     
     def __str__(self):
-        return f"{self.student_name} - {self.amount_paid} KES"
+        return f"{self.student.full_name} paid {self.amount_paid} KES"
 
 class FeeBalance(models.Model):
     parent = models.OneToOneField(User, on_delete=models.CASCADE, limit_choices_to={'role': 'parent'})
