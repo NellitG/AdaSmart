3fef43590985ea3a7d8e148ca004d5e5f061c51d
NellitG <kamaunelson157@gmail.com>
2025-07-11 17:10:24 +0300

    add function to calculate total_paid and balance

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import User, Student, Parent
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.urls import reverse
+from django.utils.html import format_html
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -19,6 +21,13 @@ class StudentAdmin(admin.ModelAdmin):
 
     def total_paid(self, obj):
         return obj.total_paid
+    total_paid.short_description = 'Total Paid'
 
     def balance(self, obj):
         return obj.balance
+    balance.short_description = 'Balance'
+
+    def download_balance_pdf(self, obj):
+        url = reverse('balance_pdf', args=[obj.id])
+        return format_html(f'<a class="button" href="{url}" target="_blank" >Download Balance PDF</a>')
+    download_balance_pdf.short_description = 'Download Balance PDF'
