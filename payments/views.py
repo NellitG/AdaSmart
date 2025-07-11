387bf259c9fbebf3d48fd3409ca9b2a25fa1b590
NellitG <kamaunelson157@gmail.com>
2025-07-11 17:14:32 +0300

    add function that generates a PDF file showing a student's balance

--- payments/views.py
+++ payments/views.py
@@ -1,37 +1,22 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from users.models import User
-from .models import FeePayment
-from notifications.services import send_sms_notification
-from .utils import generate_receipt_pdf
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
+from users.models import Student
 
+def student_balance_pdf(request, student_id):
+    student = Student.objects.get(id=student_id)
+    total_paid = sum(payment.amount_paid for payment in student.feepayment_set.all())
+    total_fee = student.total_fee 
+    balance = total_fee - total_paid
 
-def test_payment_entry(request):
-    try:
-        parent = User.objects.filter(role='parent').first()
-        if not parent:
-            return JsonResponse({'error': 'No parent user found'}, status=404)
-        
-        payment = FeePayment.objects.create(
-            parent=parent,
-            student_name = "Test Student",
-            amount_paid = 1000,
-            payment_method = 'MPesa',
-        )
+    html = render_to_string("", {
+        "student": student,
+        "total_paid": total_paid,
+        "total_fee": total_fee,
+        "balance": balance,
+    })
 
-        # Generate PDF and store the receipt URL
-        pdf_relative_path = generate_receipt_pdf(payment)
-        payment.receipt_url = f"media/{pdf_relative_path}"
-        payment.save()
-
-        # Send SMS with receipt link
-        full_url = f"http://127.0.0.1:8000/{payment.receipt_url}"
-        msg = f"Dear Parent, we have received your payment of Ksh {payment.amount_paid} for {payment.student_name}. You can download the receipt here: {full_url}"
-        send_sms_notification(parent.phone_number, msg)
-
-        return JsonResponse({
-            'status': 'success',
-            'receipt': full_url})
-    
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = f"inline; filename=balance_{student.id}.pdf"
+    weasyprint.HTML(string=html).write_pdf(response)
+    return response
