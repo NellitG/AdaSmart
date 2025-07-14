8722704773c2cf1c073bc6613e120c66d18de4de
NellitG <kamaunelson157@gmail.com>
2025-07-14 11:58:52 +0300

    remove students in the role choices

--- users/models.py
+++ users/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Admin'),
-        ('student', 'Student'),
         ('parent', 'Parent'),
     )
     name = models.CharField(max_length=100, blank=True, null=True)
